a987cfde785a48c1efad31b2baca09d452d34623
Sachin kumar <sachin67.dev@gmail.com>
2024-06-10 16:49:39 +0530

    Add loader

--- src/components/global/loader.tsx
+++ src/components/global/loader.tsx
@@ -6,18 +6,16 @@ type LoaderProps = {
   loading: boolean
   children: React.ReactNode
   className?: string
-  noPadding?: boolean
 }
 
 export const Loader = ({
   loading,
   children,
-  noPadding,
   className,
 }: LoaderProps) => {
   return loading ? (
     <div className={cn(className || 'w-full py-5 flex justify-center')}>
-      <Spinner noPadding={noPadding} />
+      <Spinner  />
     </div>
   ) : (
     children
